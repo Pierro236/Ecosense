91ab54d9ba34a73b822be6ba091110493719bfb3
jugodfroy <julien.godfroy@isep.fr>
2021-11-16 23:18:15 +0100

    adding all files

--- index.php
+++ index.php
@@ -53,37 +53,6 @@ if ($_GET) {
 
 
     <div id="container">
-<<<<<<< HEAD
-           
-            
-            <form id="login" action="verification.php" method="POST">
-                <div id="headerform">
-                    <p><?php echo $lang['Login']; ?></p>
-                    <img src="img/logo.png" alt="logo">
-                    
-            
-                </div>
-                
-                <label><b>Identifiant</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-
-                <label><b><?php echo $lang['mdp']; ?></b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
-                <input type="submit" name="Btncx"  value='Connexion' >
-                
-            </form>
-          
-    </div>
-    <div>
-     <footer>
-        <h2 class="CGU">conditions générales d'utilisation</h2>
-     </footer>
-    </div>
-   
-    
-    
-=======
 
 
         <form id="login" action="php/home.php" method="POST">
@@ -103,14 +72,16 @@ if ($_GET) {
             <input type="submit" name="Btncx" value='Connexion'>
 
         </form>
-        <footer class="footer">
-            <h2 class="CGU">Conditions générales d'utilisation</h2>
+
+    </div>
+    <div>
+        <footer>
+            <h2 class="CGU">conditions générales d'utilisation</h2>
         </footer>
     </div>
 
 
 
->>>>>>> 637139c1d8ea5b95550fc861094ba7928f08e82b
 </body>
 
 
