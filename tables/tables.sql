c4bbb65b00a4c824960d6298cd5db768f04123b9
GabyHeStylish <gl1.hercaud@gmail.com>
2021-12-14 17:58:05 +0100

    Update tables.sql
    update pour que ça marche en mysql

--- tables/tables.sql
+++ tables/tables.sql
@@ -1,48 +1,48 @@
 CREATE TABLE Capteur (
-    [Capteur ID]   INTEGER       PRIMARY KEY,
-    [Type Capteur] VARCHAR (255) 
+    CapteurID   INTEGER       PRIMARY KEY,
+    TypeCapteur VARCHAR (255) 
 );
 
 
 -- Table: Freq Cardiaque
 CREATE TABLE FreqCardiaque (
-    [Freq ID]          INTEGER      PRIMARY KEY,
-    [User ID]          INTEGER      REFERENCES Utilisateur ([User ID]),
-    [Valeur Frequence] FLOAT (255),
-    [Heure Recue Freq] DATETIME
+    FreqID              INTEGER      PRIMARY KEY,
+    UserID              INTEGER      REFERENCES Utilisateur (UserID),
+    ValeurFrequence     FLOAT (255),
+    HeureRecueFreq      DATETIME
 );
 
 
 -- Table: Mesures
 CREATE TABLE Mesures (
-    [Mesure ID]             INTEGER  PRIMARY KEY,
-    [Capteur ID]            INTEGER  REFERENCES Capteur ([Capteur ID]),
-    [Freq ID]               INTEGER  REFERENCES FreqCardiaque ([Freq ID]),
-    [Valeur Recue Salle]    FLOAT (255),
-    [Heure Reception Salle] DATETIME
+    MesureID         	    INTEGER     PRIMARY KEY,
+    CapteurID    	        INTEGER     REFERENCES Capteur (CapteurID),
+    FreqID           	 	INTEGER     REFERENCES FreqCardiaque (FreqID),
+    ValeurRecueSalle  	    FLOAT (255),
+    HeureReceptionSalle     DATETIME
 );
 
 
 -- Table: Role
 CREATE TABLE Rol (
-    [Role ID]   INTEGER       PRIMARY KEY,
-    [Role Name] VARCHAR (255) 
+    RoleID                  INTEGER       PRIMARY KEY,
+    RoleName                VARCHAR (255) 
 );
 
 
 -- Table: Room
 CREATE TABLE Room (
-    [Room ID]   INTEGER       PRIMARY KEY,
-    [Room Name] VARCHAR (255) 
+    RoomID                  INTEGER       PRIMARY KEY,
+    RoomName                VARCHAR (255) 
 );
 
 
 -- Table: Utilisateur
 CREATE TABLE Utilisateur (
-    [User ID]         INTEGER       PRIMARY KEY,
-    [Role ID]         INTEGER       REFERENCES Rol ([Role ID]),
-    [User Email]      VARCHAR (255),
-    [User Last Name]  VARCHAR (255),
-    [User First Name] VARCHAR (255),
-    [User Password]   VARCHAR (255) 
+    UserID        INTEGER       PRIMARY KEY,
+    RoleID        INTEGER       REFERENCES Rol (RoleID),
+    UserEmail      VARCHAR (255),
+    UserLastName  VARCHAR (255),
+    UserFirstName VARCHAR (255),
+    UserPassword   VARCHAR (255) 
 );
