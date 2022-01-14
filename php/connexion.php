ef64564d9967c980c4e423abe46925143bb99301
Hascoat <francoishascoat@gmail.com>
2022-01-14 17:04:43 +0100

    ajout du unhash pour mdp lors connexion

--- php/connexion.php
+++ php/connexion.php
@@ -18,7 +18,8 @@
             if(filter_var($user_email, FILTER_VALIDATE_EMAIL))
             {
                 
-                if($data['user_password'] === $user_password)
+                //if($data['user_password'] === $user_password)
+                if( password_verify($user_password, $data['user_password']))
                 {
                     $_SESSION['user_email'] = $user_email; // la session peut être appelée différemment et son contenu aussi peut être autre chose que l'identifiant'
                     header ('Location:home.php');
