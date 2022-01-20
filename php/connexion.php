18aaa1d8be66a1736078b774f5594c7169f26f8c
jugodfroy <julien.godfroy@isep.fr>
2022-01-20 18:41:55 +0100

    Fix redirection script php v2

--- php/connexion.php
+++ php/connexion.php
@@ -1,32 +1,28 @@
 <?php
-    session_start();
+session_start();
 
-    require_once 'config.php' ;
+require_once 'config.php';
 
-    if(isset($_POST['email']) && isset($_POST['password']))
-    {
-        $user_email = $_POST['email'];
-        $user_password = $_POST['password'];
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $user_email = $_POST['email'];
+    $user_password = $_POST['password'];
 
-        $check = $db->prepare("SELECT user_email, user_password FROM utilisateur WHERE user_email = '$user_email'");
-        $check->execute(array($user_email));
-        $data = $check->fetch();
-        $row = $check->rowCount();
+    $check = $db->prepare("SELECT user_email, user_password FROM utilisateur WHERE user_email = '$user_email'");
+    $check->execute(array($user_email));
+    $data = $check->fetch();
+    $row = $check->rowCount();
 
-        if($row == 1)
-        {
-            if(filter_var($user_email, FILTER_VALIDATE_EMAIL))
-            {
-                
-                //if($data['user_password'] === $user_password)
-                if( password_verify($user_password, $data['user_password']))
-                {
-                    $_SESSION['user_email'] = $user_email; // la session peut être appelée différemment et son contenu aussi peut être autre chose que l'identifiant'
-                    header ('Location:home.php');
-                
-                }else echo "Mot de passe incorrect";
-            }else echo "Adresse user_email non reconnue";
-        }else echo "Compte non existant";
-    } else header('Location:index.php');
+    if ($row == 1) {
+        if (filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
 
-?>
+            //if($data['user_password'] === $user_password)
+            if (password_verify($user_password, $data['user_password'])) {
+                $_SESSION['user_email'] = $user_email; // la session peut être appelée différemment et son contenu aussi peut être autre chose que l'identifiant'
+                header('Location:home.php');
+            } else echo "Mot de passe incorrect";
+        } else echo "Adresse user_email non reconnue";
+    } else {
+        header('Location: ../index.php');
+        echo "Compte non existant";
+    }
+} else header('Location:index.php');
